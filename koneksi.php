ca6ad10973994c4d35fb8f47c9cfb8b05a2a0085
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2026-02-07 18:58:20 +0700

    refactor : memberikan komentar $connection

--- koneksi.php
+++ koneksi.php
@@ -9,7 +9,7 @@ $db_name = "db_g_tailor";
 $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if ($connection) {
-    echo "Koneksi Berhasil!";
+    // echo "Koneksi Berhasil!";
 } else {
     echo "Koneksi Gagal: " . mysqli_connect_error();
 }
